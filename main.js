22135530880b8de7f9bbe67220107594b055b0bb
Radina <49740981+radinag@users.noreply.github.com>
2019-05-29 02:00:25 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -28,41 +28,4 @@ function imgToggle(e) {
 		// add it
 		pic.classList.add('isHidden');
 	}
-
 }
-//////////////////////////////////////////////////////////////////////////////////////////
-// [ Bonus ]
-// Instead of creating a variable for each item on the screen, select them all at once and add an event listener to each one in a loop.
-
-// const characters = ['Fry', 'Leela', 'Bender', 'Professor', 'Amy', 'Zapp', 'Zoidberg', 'Mom'];
-
-// var toggledChar = document.querySelectorAll('button');
-// toggledChar.addEventListener('click',imgToggle );
-
-// for (let i=0; i < characters.length; i++ ) {
-// 	console.log(characters[i]);
-	
-// }
-
-
- 
-// //  button.addEventListener('click', imgToggle);
-
-// //  toggledImg.addEventListener('click', imgToggle);
-         
-
-// function imgToggle(e) {
-// 	var pic = document.getElementById( this.className );
-
-// 	// check to see if the element has a class of 'isHidden'
-// 	if (pic.classList.contains('isHidden')) {
-// 		// remove it
-// 		pic.classList.remove('isHidden');
-// 	} else {
-// 		pic.classList.add('isHidden');
-// 		// add it
-// 	}
-
-// }
-
-//////////////////////////////////////////////////////////////////////////////////////////
